a3c297a623816e13b4917d515e7fd9cd802a92ec
= <phuongnguyen20904@gmail.com>
2024-09-10 23:27:40 +0700

    First commit: add base

---
+++ src/main/GamePanel.java
@@ -0,0 +1,90 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+    //SCREEN SETTINGS
+    private final int originalTileSize = 16;
+    private final int scale = 3;
+    public int tileSize = originalTileSize * scale; //58x58
+    private final int maxScreenCol = 16;
+    private final int maxScreenRow = 16;
+    public int screenWidth = maxScreenCol * tileSize;//1160 px
+    public int screenHeight = maxScreenRow * tileSize;//696px
+
+    //WORLD SETTINGS
+    public final int maxMap = 1;
+    public int currentMap = 0;
+    public final int maxWorldCol = 16;
+    public final int maxWorldRow = 16;
+    public final int worldWidth = maxWorldCol * tileSize;//2400
+    public final int worldHeight = maxWorldRow * tileSize;//2400
+
+    // SETUP FPS
+    int FPS = 60;
+
+    //SYSTEM
+    Thread gameThread;
+
+    //SET UP MAP
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+//        this.addKeyListener(keyHandler);
+        this.setVisible(true);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        double drawInterval = (double) 1000000000 / FPS;
+
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        //display FPS
+        long timer = 0;
+        int drawCount = 0;
+
+        while (gameThread != null) //as long as gameThread is existed
+        {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if (timer >= 1000000000) {
+//                System.out.println("FPS" + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+
+    public void update(){}
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.dispose();
+    }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import javax.swing.*;
+public class Main {
+    public static JFrame window;
+    public static void main(String[] args) {
+        window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setTitle("2D Adventure");
+//        window.setUndecorated(true);    //remove top bar
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack(); //
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+//        gamePanel.setupGame();
+        gamePanel.startGameThread();
+    }
+}
