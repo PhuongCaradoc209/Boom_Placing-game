a05eae7496d655074e8a29ae33772cb0ceb3754c
= <phuongnguyen20904@gmail.com>
2024-09-11 19:39:29 +0700

    Add: Entity This will manage all the entity in the game

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,212 +1,188 @@
-//package entity;
-//
-//import main.GamePanel;
-//import main.UtilityTool;
-//
-//import javax.imageio.ImageIO;
-//import javax.swing.plaf.PanelUI;
-//import java.awt.*;
-//import java.awt.image.BufferedImage;
-//import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.Objects;
-//
-//public class Entity {
-//    public GamePanel gp;
-//    protected int size;
-//    public double worldX, worldY;
-//    public double speed;
-//    public BufferedImage up1, up2, down1, down2, right1, right2, left1, left2,
-//            standUp, standDown, standRight, standLeft;
-//    public String direction = "down";
-//    public int spriteCounter = 0;
-//    public int spriteNum = 1;
-//    public int solidAreaDefaultX, solidAreaDefaultY;
-//    public Rectangle solidArea;// by this, you can manage which part of tile can be collision
-//    public boolean collisionOn = false;
-//    public int actionLookCounter = 0;
-//
-//    // DIALOG
-//    public String dialogues[] = new String[20];
-//    public int dialogueIndex = 0;
-//
-//    public Entity(GamePanel gp) {
-//        this.gp = gp;
-//        solidArea = new Rectangle(0, 0, gp.tileSize, gp.tileSize);
-//    }
-//
-//    public void setAction() {
-//    }
-//
-//    public void update() {
-//        setAction();
-//
-////        if (gp.currentMap == 0) {
-////            gp.cChecker.checkObj(this, false);
-////            gp.cChecker.checkEntity(this, gp.npc);
-////            gp.cChecker.checkEntity(this, gp.animal);
-////            gp.cChecker.checkPlayer(this);
-////        }
-////        gp.cChecker.checkTile(this, isDuck);
-////        gp.cChecker.checkAtEdge(this);
-//
-//        // IF COLLISION IS FALSE, PLAYER CAN MOVE
+package entity;
+
+import main.GamePanel;
+import main.UtilityTool;
+
+import javax.imageio.ImageIO;
+import javax.swing.plaf.PanelUI;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Entity {
+    public GamePanel gp;
+    protected int size;
+    public double worldX, worldY;
+    public double speed;
+    public BufferedImage
+            up1, up2, up3, up4,
+            down1, down2, down3, down4,
+            right1, right2 , right3, right4,
+            left1, left2, left3, left4,
+            standUp1, standUp2, standUp3, standUp4,
+            standDown1, standDown2, standDown3, standDown4;
+    public String direction = "down";
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
+    public int solidAreaDefaultX, solidAreaDefaultY;
+    public Rectangle solidArea;// by this, you can manage which part of tile can be collision
+    public boolean collisionOn = false;
+    public int actionLookCounter = 0;
+
+    public Entity(GamePanel gp) {
+        this.gp = gp;
+        solidArea = new Rectangle(0, 0, gp.tileSize, gp.tileSize);
+    }
+
+    public void setAction() {
+    }
+
+    public void update() {
+        setAction();
+
+//        if (gp.currentMap == 0) {
+//            gp.cChecker.checkObj(this, false);
+//            gp.cChecker.checkEntity(this, gp.npc);
+//            gp.cChecker.checkEntity(this, gp.animal);
+//            gp.cChecker.checkPlayer(this);
+//        }
+//        gp.cChecker.checkTile(this, isDuck);
+//        gp.cChecker.checkAtEdge(this);
+
+        // IF COLLISION IS FALSE, PLAYER CAN MOVE
 //        if (!collisionOn) {
-//            // System.out.println(direction);
-//            switch (direction) {
-//                case "up":
-//                    worldY -= speed;
-//                    break;
-//                case "down":
-//                    worldY += speed;
-//                    break;
-//                case "right":
-//                    worldX += speed;
-//                    break;
-//                case "left":
-//                    worldX -= speed;
-//                    break;
-//                case "diagonalUpLeft":
-//                    worldX -= speed;
-//                    worldY -= speed;
-//                    break;
-//                case "diagonalUpRight":
-//                    worldX += speed;
-//                    worldY -= speed;
-//                    break;
-//                case "diagonalDownLeft":
-//                    worldX -= speed;
-//                    worldY += speed;
-//                    break;
-//                case "diagonalDownRight":
-//                    worldX += speed;
-//                    worldY += speed;
-//                    break;
-//            }
+            // System.out.println(direction);
+            switch (direction) {
+                case "up":
+                    worldY -= speed;
+                    break;
+                case "down":
+                    worldY += speed;
+                    break;
+                case "right":
+                    worldX += speed;
+                    break;
+                case "left":
+                    worldX -= speed;
+                    break;
+            }
 //        }
-//
-//        spriteCounter++;
-//        if (spriteCounter > 20) {
-//            spriteNum = (spriteNum == 2) ? 1 : 2;
-//            spriteCounter = 0;
+
+        spriteCounter++;
+        if (spriteCounter > 15) {
+            if (spriteNum == 1)
+                spriteNum = 2;
+            else if (spriteNum == 2)
+                spriteNum = 3;
+            else if (spriteNum == 3)
+                spriteNum = 4;
+            else if (spriteNum == 4)
+                spriteNum = 1;
+            spriteCounter = 0;
+        }
+    }
+
+    public void draw(Graphics2D g2) {
+        BufferedImage image = null;
+        double screenX = worldX - gp.player.worldX + gp.player.screenX;
+        double screenY = worldY - gp.player.worldY + gp.player.screenY;
+
+        // STOP MOVING THE CAMERA AT EDGE (ENTITY CAN NOT MOVE IF AT EDGE)
+        // TOP
+        if (gp.player.screenX >= gp.player.worldX) {
+            screenX = worldX;
+        }
+        // LEFT
+        if (gp.player.screenY >= gp.player.worldY) {
+            screenY = worldY;
+        }
+        // RIGHT
+        double rightOffSet = gp.screenWidth - gp.player.screenX;
+        if (rightOffSet >= gp.worldWidth - gp.player.worldX) {
+            screenX = gp.screenWidth - (gp.worldWidth - worldX);
+        }
+        // BOTTOM
+        double bottomOffSet = gp.screenHeight - gp.player.screenY;
+        if (bottomOffSet >= gp.worldHeight - gp.player.worldY) {
+            screenY = gp.screenHeight - (gp.worldHeight - worldY);
+        }
+        ////////////////////////
+        switch (direction) {
+            case "up":
+                if (spriteNum == 1) {
+                    image = up1;
+                }
+                if (spriteNum == 2) {
+                    image = up2;
+                }
+                break;
+            case "down":
+                if (spriteNum == 1) {
+                    image = down1;
+                }
+                if (spriteNum == 2) {
+                    image = down2;
+                }
+                break;
+            case "left":
+                if (spriteNum == 1) {
+                    image = left1;
+                }
+                if (spriteNum == 2) {
+                    image = left2;
+                }
+                break;
+            case "right":
+                if (spriteNum == 1) {
+                    image = right1;
+                }
+                if (spriteNum == 2) {
+                    image = right2;
+                }
+                break;
+        }
+        // IF PLAYER AT THE EDGE
+//        if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
+//                worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+//                worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
+//                worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
+//            g2.drawImage(image, (int) screenX, (int) screenY, size, size, null);
+//        } else if (gp.player.screenX > gp.player.worldX ||
+//                gp.player.screenY > gp.player.worldY ||
+//                rightOffSet > gp.worldWidth - gp.player.worldX ||
+//                bottomOffSet > gp.worldHeight - gp.player.worldY) {
+        g2.drawImage(image, (int) screenX, (int) screenY, size, size, null);
 //        }
-//    }
-//
-//    public void draw(Graphics2D g2) {
-//        BufferedImage image = null;
-//        double screenX = worldX - gp.player.worldX + gp.player.screenX;
-//        double screenY = worldY - gp.player.worldY + gp.player.screenY;
-//
-//        // STOP MOVING THE CAMERA AT EDGE (ENTITY CAN NOT MOVE IF AT EDGE)
-//        // TOP
-//        if (gp.player.screenX >= gp.player.worldX) {
-//            screenX = worldX;
-//        }
-//        // LEFT
-//        if (gp.player.screenY >= gp.player.worldY) {
-//            screenY = worldY;
-//        }
-//        // RIGHT
-//        double rightOffSet = gp.screenWidth - gp.player.screenX;
-//        if (rightOffSet >= gp.worldWidth - gp.player.worldX) {
-//            screenX = gp.screenWidth - (gp.worldWidth - worldX);
-//        }
-//        // BOTTOM
-//        double bottomOffSet = gp.screenHeight - gp.player.screenY;
-//        if (bottomOffSet >= gp.worldHeight - gp.player.worldY) {
-//            screenY = gp.screenHeight - (gp.worldHeight - worldY);
-//        }
-//        ////////////////////////
-//        switch (direction) {
-//            case "up":
-//                if (spriteNum == 1) {
-//                    image = up1;
-//                }
-//                if (spriteNum == 2) {
-//                    image = up2;
-//                }
-//                break;
-//            case "down":
-//                if (spriteNum == 1) {
-//                    image = down1;
-//                }
-//                if (spriteNum == 2) {
-//                    image = down2;
-//                }
-//                break;
-//            case "left":
-//                if (spriteNum == 1) {
-//                    image = left1;
-//                }
-//                if (spriteNum == 2) {
-//                    image = left2;
-//                }
-//                break;
-//            case "right":
-//                if (spriteNum == 1) {
-//                    image = right1;
-//                }
-//                if (spriteNum == 2) {
-//                    image = right2;
-//                }
-//                break;
-//            case "diagonalUpLeft":
-//                if (spriteNum == 1) {
-//                    image = diagonal_up_left1;
-//                }
-//                if (spriteNum == 2) {
-//                    image = diagonal_up_left2;
-//                }
-//                break;
-//            case "diagonalUpRight":
-//                if (spriteNum == 1) {
-//                    image = diagonal_up_right1;
-//                }
-//                if (spriteNum == 2) {
-//                    image = diagonal_up_right2;
-//                }
-//                break;
-//            case "diagonalDownLeft":
-//                if (spriteNum == 1) {
-//                    image = diagonal_down_left1;
-//                }
-//                if (spriteNum == 2) {
-//                    image = diagonal_down_left2;
-//                }
-//                break;
-//            case "diagonalDownRight":
-//                if (spriteNum == 1) {
-//                    image = diagonal_down_right1;
-//                }
-//                if (spriteNum == 2) {
-//                    image = diagonal_down_right2;
-//                }
-//                break;
-//        }
-//        // IF PLAYER AT THE EDGE
-////        if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
-////                worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
-////                worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
-////                worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
-////            g2.drawImage(image, (int) screenX, (int) screenY, size, size, null);
-////        } else if (gp.player.screenX > gp.player.worldX ||
-////                gp.player.screenY > gp.player.worldY ||
-////                rightOffSet > gp.worldWidth - gp.player.worldX ||
-////                bottomOffSet > gp.worldHeight - gp.player.worldY) {
-//        g2.drawImage(image, (int) screenX, (int) screenY, size, size, null);
-////        }
-//    }
-//
-//    public BufferedImage setup(String imagePath, int width, int height) {
-//        UtilityTool uTool = new UtilityTool();
-//        BufferedImage image = null;
-//        try {
-//            image = ImageIO
-//                    .read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(imagePath + ".png")));
-//            image = uTool.scaleImage(image, width, height);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return image;
-//    }
-//}
+    }
+
+    public BufferedImage setup(String imagePath, int targetWidth, int targetHeight) {
+        UtilityTool uTool = new UtilityTool();
+        BufferedImage image = null;
+        try {
+            image = ImageIO
+                    .read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(imagePath + ".png")));
+            // Get original dimensions
+            int originalWidth = image.getWidth();
+            int originalHeight = image.getHeight();
+
+            // Calculate aspect ratio
+            double aspectRatio = (double) originalWidth / originalHeight;
+
+            // Adjust target width or height to maintain aspect ratio
+            if (originalWidth > originalHeight) {
+                targetHeight = (int) (targetWidth / aspectRatio);
+            } else {
+                targetWidth = (int) (targetHeight * aspectRatio);
+            }
+
+            // Resize the image while maintaining aspect ratio
+            image = uTool.scaleImage(image, targetWidth, targetHeight);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+}
