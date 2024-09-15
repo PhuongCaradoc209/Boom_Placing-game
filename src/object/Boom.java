4275535d80e22dcca86d6d6afe32cb88a325e641
= <phuongnguyen20904@gmail.com>
2024-09-15 11:07:42 +0700

    Debug draw method of boom

--- src/object/Boom.java
+++ src/object/Boom.java
@@ -7,27 +7,26 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Boom extends Entity {
-    private int x,y;
     private boolean exploded;
     BufferedImage[] boomAnim;
     private int frameBoom;
-    private int intervalBoom = 24;
+    private int intervalBoom = 14;
     private int currentFrame = 0;
     private int countToExplode = 0;
-    private int maxCountToExplode = 4; // Số lần lặp lại toàn bộ animation
+    private int maxCountToExplode = 6; // Số lần lặp lại toàn bộ animation
 
-    public Boom(int x, int y, int countToExplode, GamePanel gp){
+    public Boom(int col, int row, int countToExplode, GamePanel gp) {
         super(gp);
         boomAnim = new BufferedImage[3];
-        this.x = x;
-        this.y = y;
+        this.worldX = gp.tileSize * col + 10;
+        this.worldY = gp.tileSize * row + 10;
         this.exploded = false;
         setUpImage();
     }
 
-    private void setUpImage(){
-        for(int i = 0; i < 3; i++){
-            boomAnim[i] = setup("object/boom_" + (i + 1), 3*gp.tileSize/4,3*gp.tileSize/4);
+    private void setUpImage() {
+        for (int i = 0; i < 3; i++) {
+            boomAnim[i] = setup("object/boom_" + (i + 1), 3 * gp.tileSize / 4, 3 * gp.tileSize / 4);
         }
     }
 
@@ -48,27 +47,37 @@ public class Boom extends Entity {
     }
 
     public void draw(Graphics2D g2) {
-        g2.drawImage(boomAnim[currentFrame], x, y, null);
+        if (exploded) {
+
+        } else {
+            double screenX = worldX - gp.player.worldX + gp.player.screenX;
+            double screenY = worldY - gp.player.worldY + gp.player.screenY;
+
+            // STOP MOVING THE CAMERA AT EDGE (ENTITY CAN NOT MOVE IF AT EDGE)
+            // TOP
+            if (gp.player.screenX >= gp.player.worldX) {
+                screenX = worldX;
+            }
+            // LEFT
+            if (gp.player.screenY >= gp.player.worldY) {
+                screenY = worldY;
+            }
+            // RIGHT
+            double rightOffSet = gp.screenWidth - gp.player.screenX;
+            if (rightOffSet >= gp.worldWidth - gp.player.worldX) {
+                screenX = gp.screenWidth - (gp.worldWidth - worldX);
+            }
+            // BOTTOM
+            double bottomOffSet = gp.screenHeight - gp.player.screenY;
+            if (bottomOffSet >= gp.worldHeight - gp.player.worldY) {
+                screenY = gp.screenHeight - (gp.worldHeight - worldY);
+            }
+            g2.drawImage(boomAnim[currentFrame],  (int) screenX, (int) screenY, null);
+        }
     }
 
     //Explode
     public boolean isExplode() {
         return exploded;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
     }
 }
